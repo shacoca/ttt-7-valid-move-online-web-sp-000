6736c34d9f9d93812a30d6b0cdd78a83597e0974
shawn campbell <shacoca@yahoo.com>
2020-07-29 19:55:22 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,27 @@
-# code your #valid_move? method here
+def valid_move?(board, index)
+  if (input_in_range?(index) && (!(position_taken?(board, index))))
+    true
+  end
+end
 
+def position_taken?(board, index)
+  square = board[index]
+#
+  if square == " "
+    false
+  elsif square == ""
+    false
+  elsif square == nil
+    false
+  elsif square == "X"
+    true
+  elsif square == "O"
+    true
+  end
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def input_in_range?(index)
+  if index.between?(0, 8)
+    true
+  end
+end
