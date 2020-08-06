1a35ef41fa72f273f308c435bc0fe93b5e723ba8
shawn campbell <shacoca@yahoo.com>
2020-08-06 02:40:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 def valid_move?(board, index)
   if (input_in_range?(index) && (!(position_taken?(board, index))))
+=======
+def valid_move?(board, move)
+  if (input_in_range?(move) && (!(position_taken?(board, move - 1))))
+>>>>>>> 4fb0e2a1b015131a8942050e92fd702c13803735
     true
   end
 end
@@ -20,8 +25,14 @@ def position_taken?(board, index)
   end
 end
 
+<<<<<<< HEAD
 def input_in_range?(index)
   if index.between?(0, 8)
+=======
+def input_in_range?(player_selection)
+  number = player_selection.to_i
+  if player_selection.between?(1, 9)
+>>>>>>> 4fb0e2a1b015131a8942050e92fd702c13803735
     true
   end
 end
